dec0fa83166db93b06a8f55a16af40d987b44f65
MisterCruz <ID+username@users.noreply.github.com>
2022-07-15 15:09:53 -0400

    initial commit

---
+++ src/MealGenerator.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import YoutubeEmbed from './YoutubeEmbed';
+import './MealGenerator.css'
+
+class MealGenerator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            meals: [],
+            isLoaded: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    meals: data.meals[0],
+                    isLoaded: true
+                })
+            })
+    }
+
+    render() {
+        const { meals } = this.state;
+        return (
+            <div> 
+                <button onClick={this.handleClick}>Get new meal</button>
+                {this.state.isLoaded && <h2>Meal Name: {meals.strMeal} </h2>}
+                <br />
+                <img src={meals.strMealThumb} />
+                <br />
+                {this.state.isLoaded && <h2 className="recipe">Recipe: {meals.strInstructions}</h2> }
+            </div>
+        )
+    }
+}
+
+export default MealGenerator;
